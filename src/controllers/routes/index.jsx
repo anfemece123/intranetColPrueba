11a36fbe921e32cffd717cad19b561536a27f5df
DanielaGomez <ledangom@gmail.com>
2023-11-09 18:13:12 -0500

    Se modifica la manera de hacer las rutas a las diferentes paginas y componentes

--- src/controllers/routes/index.jsx
+++ src/controllers/routes/index.jsx
@@ -1,35 +1,49 @@
-import { createBrowserRouter } from "react-router-dom";
+import { Route, Routes, createBrowserRouter } from "react-router-dom";
 import MiniDrawer from "../../components/SideBar";
 import Home from "../../screens/Home";
 import TablasBasicas from "../../screens/TablasBasicas";
 import SignInSide from "../../screens/SignInSide";
 import Usuarios from "../../screens/Usuarios";
+import { AppColores } from "../../color/AppColores";
+
+export const AppRouter = () => {
+  return (
+    <AppColores>
+      <Routes>
+        <Route path="/" element={<MiniDrawer />} />
+        <Route path="/Home" element={<Home />} />
+        <Route path="/TablasBasicas" element={<TablasBasicas />} />
+        <Route path="/Login" element={<SignInSide />} />
+        <Route path="/Usuarios" element={<Usuarios />} />
+      </Routes>
+    </AppColores>
+  )
+}
 
 
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <MiniDrawer />,
+//     // errorElement: <ErrorPage />,
+//   },
+//   {
+//     path: "/Home",
+//     element: <Home />,
+//   },
+//   {
+//     path: "/TablasBasicas",
+//     element: <TablasBasicas />,
+//   },
+//   {
+//     path: "/Login",
+//     element: <SignInSide />,
+//   },
+//   {
+//     path: "/Usuarios",
+//     element: <Usuarios />,
+//   },
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <MiniDrawer />,
-    // errorElement: <ErrorPage />,
-  },
-    {
-      path: "/Home",
-      element: <Home />,
-    },
-    {
-      path: "/TablasBasicas",
-      element: <TablasBasicas />,
-    },
-    {
-      path: "/Login",
-      element: <SignInSide />,
-    },
-    {
-      path: "/Usuarios",
-      element: <Usuarios />,
-    },
-   
-  ]);
-  
-  export default router;
+// ]);
+
+// export default router;
